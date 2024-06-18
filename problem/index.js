220152b35a55d5bcafb24ba4c95b79eab044956a
Akash Agrawal <agrawalakash82@gmail.com>
2024-06-18 16:23:42 +0530

    adding in github

---
+++ problem/index.js
@@ -0,0 +1,24 @@
+// Please don't change the pre-written code
+// Import the necessary modules here
+import tweetRoutes from "./src/features/tweet/tweet.routes.js";
+import express from "express";
+import {
+  getTweets,
+  createTweet,
+} from "./src/features/tweet/tweet.controller.js";
+const app = express();
+
+// TODO: Refactor these route handlers into tweet.routes.js file using express Router --------->>>>
+// app.get("/api/tweets", getTweets);
+
+// app.post("/api/tweets", createTweet);
+// Write your code here
+
+// -------------->>>>>>>>>>>>>>>>>>>
+
+// middleware for accessing these routes after refactoring
+app.use("/api/tweets", tweetRoutes);
+
+app.listen(5000, () => {
+  console.log("server is listening at port 5000");
+});
